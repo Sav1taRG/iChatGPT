395d6b5466263aa3f3b2f24e78a7639036dc264a
Sav1taRG <52829649+Sav1taRG@users.noreply.github.com>
2023-03-21 12:42:20 +0200

    token swift file gitignore

---
+++ ChatGPT/ChatGptToken.swift
@@ -0,0 +1,10 @@
+//
+//  ChatGptToken.swift
+//  ChatGPT
+//
+//  Created by Roman Golubinko on 05.03.2023.
+//
+
+enum Token: String {
+    case myToken = "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"
+}
